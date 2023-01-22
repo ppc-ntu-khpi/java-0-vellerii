f233ed713b25ac67a875a3c0fbb991b22b5e606f
Valeriia Krychevska <90756841+vellerii@users.noreply.github.com>
2023-01-22 15:09:14 +0200

    Done on "5" grading

--- src/Shirt.java
+++ src/Shirt.java
@@ -3,15 +3,15 @@ public class Shirt {
   public int shirtID = 4506; // стандартне значення номера моделі сорочки
   public String description = "A shirt made from materials that are comfortable to wear and durable, yet easy to care for."; // стандартний опис сорочки
   // коди кольорів: R=червоний, B=синій, G=зелений, U=невідомо
-  public char colorCode = 'U';
+  public String color = "Blue";
   public double price = 75.0; // стандартна вартість сорочки
   public int quantityInStock = 0; // стандартна кількість на складі
   
   // цей метод просто виводить всю інформацію про сорочку на екран
   public void displayShirtInformation() {
     System.out.println("Shirt ID: " + shirtID);
-    System.out.println("Shirt description:" + description);
-    System.out.println("Color Code: " + colorCode);
+    System.out.println("Shirt description: " + description);
+    System.out.println("Color: " + color);
     System.out.println("Shirt price: $" + price);
     System.out.println("Quantity in stock: " + quantityInStock);
   } // кінець методу displayShirtInformation
