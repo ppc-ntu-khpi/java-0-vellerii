af09e98d2dbf5481c764ca5e5901ffb87ac68286
Valeriia Krychevska <90756841+vellerii@users.noreply.github.com>
2023-01-22 13:37:51 +0200

    Changed shirtID, description and price

--- src/Shirt.java
+++ src/Shirt.java
@@ -1,9 +1,10 @@
 public class Shirt {
-  public int shirtID = 0; // стандартне значення номера моделі сорочки
-  public String description = "-description required-"; // стандартний опис сорочки
+    
+  public int shirtID = 4506; // стандартне значення номера моделі сорочки
+  public String description = "A shirt made from materials that are comfortable to wear and durable, yet easy to care for."; // стандартний опис сорочки
   // коди кольорів: R=червоний, B=синій, G=зелений, U=невідомо
   public char colorCode = 'U';
-  public double price = 0.0; // стандартна вартість сорочки
+  public double price = 75.0; // стандартна вартість сорочки
   public int quantityInStock = 0; // стандартна кількість на складі
   
   // цей метод просто виводить всю інформацію про сорочку на екран
